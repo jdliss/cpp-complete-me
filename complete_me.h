3cb21c33fa4bd56a30218360112f393085b2d6bb
Jonathan <jonacom@lissismore.com>
2016-09-14 17:22:17 -0400

    make recursive functions private

--- complete_me.h
+++ complete_me.h
@@ -13,11 +13,11 @@ class CompleteMe
   map<string, map<string, int>>weights;
 
   int insert(string);
-  int insertRecursive(string, int, Node*);
 
   vector<string> suggest(string);
-  vector<string> suggestSetup(string, Node*);
 
  private:
+  int insertRecursive(string, int, Node*);
+  vector<string> suggestSetup(string, Node*);
   vector<string> split(string);
 };
