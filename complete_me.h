577d0170eafbb3a1ac82157c22ca3d618493c512
Jonathan <jonacom@lissismore.com>
2016-09-14 17:20:47 -0400

    undo accidental deletion of header file

---
+++ complete_me.h
@@ -0,0 +1,22 @@
+#include "node.h"
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+class CompleteMe
+{
+ public:
+  CompleteMe();
+  Node root;
+  int count;
+  map<string, map<string, int>>weights;
+
+  int insert(string);
+  int insertRecursive(string, int, Node*);
+
+  vector<string> suggest(string);
+  vector<string> suggestSetup(string, Node*);
+
+  vector<string> split(string);
+};
